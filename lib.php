51999535ffe4a4a6e07e1bfd7d00c4f762904cdf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-04-07 18:32:53 -0700

    fixed and augmented caching implementation.

--- lib.php
+++ lib.php
@@ -520,7 +520,17 @@ function theme_ucsf_get_category_roots($id) {
     }
 
     if (!array_key_exists($id, $cache)) {
+        $ids = _theme_ucsf_get_category_roots($id);
         $cache[$id] = _theme_ucsf_get_category_roots($id);
+        array_shift($ids);
+        // cache category roots of all ancestors in that category hierarchy while at it.
+        for ($i = 0, $n = count($ids); $i < $n; $i++) {
+            $parent_id = $ids[$i];
+            if (array_key_exists($parent_id, $cache)) {
+                break;
+            }
+            $cache[$parent_id] = array_slice($ids, $i);
+        }
     }
     return $cache[$id];
 }
@@ -990,7 +1000,7 @@ function theme_ucsf_page_init(moodle_page $page) {
  * Recursively retrieve all ancestral categories for a given category, including the category itself.
  * @param int $id The category id.
  * @param array $categories A partial list of ancestral category ids.
- * @return array A list full list of ancestral category ids.
+ * @return array A list full list of ancestral category ids, including the given id itself.
  */
 function _theme_ucsf_get_category_roots($id, $categories = array()) {
     global $DB;
