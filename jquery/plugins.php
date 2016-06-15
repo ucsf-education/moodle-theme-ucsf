5be23c1f323470dd6d7aa0ed651cf24ec1bf1434
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2016-06-15 16:15:24 -0700

    bumped plugin version number.

--- jquery/plugins.php
+++ jquery/plugins.php
@@ -37,6 +37,6 @@
  */
 
 $plugins = array(
-    'alert'     => array('files' => array('bootstrap_plugins/alert_2.3.2-min.js')),
+    'alert'     => array('files' => array('bootstrap_plugins/alert_2.3.2.1-min.js')),
     'datepicker'     => array('files' => array('datepicker.js'))
 );
