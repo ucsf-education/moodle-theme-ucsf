76786a072a6f27007855437ef7502d293ebf72ea
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-04-11 14:43:01 -0700

    omit ucsf stylesheet.

--- config.php
+++ config.php
@@ -40,7 +40,7 @@ $THEME->name = 'ucsf';
 //
 $THEME->parents = array('clean');
 $THEME->doctype = 'html5';
-$THEME->sheets = array('bootstrap-datepicker', 'custom', 'ucsf');
+$THEME->sheets = array('bootstrap-datepicker', 'custom');
 $THEME->lessfile = 'moodle';
 $THEME->parents_exclude_sheets = array('bootstrapbase' => array('moodle'));
 $THEME->lessvariablescallback = 'theme_ucsf_less_variables';
