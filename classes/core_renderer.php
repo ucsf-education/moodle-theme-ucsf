84e989167bb64ab5c5f382edaf089d98c50fce2e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-04-18 14:38:45 -0700

    wrapped theme_ucsf_* functions with renderer procedures

--- classes/core_renderer.php
+++ classes/core_renderer.php
@@ -1,26 +1,24 @@
 <?php
 
 /**
- * UCSF theme with the underlying Bootstrap theme.
+ * UCSF theme core renderer.
  *
- * @package    theme
- * @subpackage UCSF
- * @author     Lambda Soulutions
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package theme_ucsf
  */
 
 require_once($CFG->dirroot . '/theme/clean/classes/core_renderer.php');
 require_once($CFG->dirroot . '/theme/ucsf/lib.php');
 
-
-class theme_ucsf_core_renderer extends theme_clean_core_renderer {
+class theme_ucsf_core_renderer extends theme_clean_core_renderer
+{
 
     /**
      * @inheritdoc
      */
-    public function custom_menu($custommenuitems = '') {
+    public function custom_menu($custommenuitems = '')
+    {
 
-        global $CFG, $COURSE, $PAGE;
+        global $CFG;
 
         if (empty($custommenuitems) && !empty($CFG->custommenuitems)) {
             $custommenuitems = $CFG->custommenuitems;
@@ -30,9 +28,9 @@ class theme_ucsf_core_renderer extends theme_clean_core_renderer {
         }
         $custommenu = new custom_menu($custommenuitems, current_language());
 
-        $categories = theme_ucsf_get_category_roots(theme_ucsf_get_current_course_category($PAGE, $COURSE));
+        $categories = $this->get_category_roots($this->get_current_course_category());
 
-        $coursecategory = theme_ucsf_find_first_configured_category($PAGE->theme->settings, $categories, 'custommenu');
+        $coursecategory = $this->find_first_configured_category($categories, 'custommenu');
 
         $themeconfig = get_config("theme_ucsf");
         $customizedmenu = "custommenu" . $coursecategory;
@@ -50,19 +48,38 @@ class theme_ucsf_core_renderer extends theme_clean_core_renderer {
     /**
      * Returns a help menu.
      *
-     * @return string HTML The help menu, or a blank string.
+     * @return string The help menu HTML, or a blank string.
      */
     public function help_menu()
     {
-        global $PAGE, $COURSE;
-
-        if (! isloggedin()) {
+        if (!isloggedin()) {
             return '';
         }
 
-        $menu = theme_ucsf_get_helpmenu($PAGE, $COURSE);
+        $theme_settings = $this->page->theme->settings;
 
-        if (! empty($menu)) {
+        $menu = false;
+
+        if ($theme_settings->enablecustomization) {
+            $current_category = $this->get_current_course_category();
+
+            if (!empty($current_category)) {
+                $parent_categories = $this->get_category_roots($current_category);
+                while (!$menu && !empty($parent_categories)) {
+                    $category = array_shift($parent_categories);
+                    $menu = $this->get_category_helpmenu($category);
+                }
+            }
+
+            if (!$menu) {
+                $menu = $this->get_default_helpmenu();
+            }
+        } else {
+            $menu = $this->get_default_helpmenu();
+        }
+
+
+        if (!empty($menu)) {
             return $this->render_from_template('theme_ucsf/helpmenu_popover', $menu);
         }
 
@@ -71,12 +88,96 @@ class theme_ucsf_core_renderer extends theme_clean_core_renderer {
     }
 
     /**
+     * Returns the default help menu data.
+     *
+     * @return array|bool
+     */
+    protected function get_default_helpmenu()
+    {
+        $theme_settings = $this->page->theme->settings;
+        if (!$theme_settings->enablehelpfeedback) {
+            return false;
+        }
+
+        $menu = array();
+
+        $title = $theme_settings->helpfeedbacktitle;
+        $menu['title'] = empty($title) ? get_string('helpmenutitle', 'theme_ucsf') : $title;
+
+        $menu['items'] = array();
+        $number_of_links = (int)$theme_settings->numberoflinks;
+        for ($i = 1; $i <= $number_of_links; $i++) {
+            $url = $this->get_theme_setting('helpfeedback' . $i . 'link');
+            $title = $this->get_theme_setting('helpfeedback' . $i . 'linklabel');
+            $target = $this->get_theme_setting('helpfeedback' . $i . 'linktarget');
+
+            if (!empty($url)) {
+                $menu['items'][] = array(
+                    'url' => $url,
+                    'title' => empty($title) ? '' : $title,
+                    'options' => array(
+                        'target' => empty($target) ? '_self' : '_blank'
+                    ),
+                );
+            }
+        }
+
+        if (empty($menu['items'])) {
+            return false;
+        }
+
+        return $menu;
+    }
+
+    /**
+     * Returns the category help menu data.
+     *
+     * @param int $category The course category id.
+     * @return array|bool
+     */
+    protected function get_category_helpmenu($category)
+    {
+        if (!$this->get_theme_setting('catenablehelpfeedback' . $category)) {
+            return false;
+        }
+
+        $menu = array();
+
+        $title = $this->get_theme_setting('cathelpfeedbacktitle' . $category);
+        $menu['title'] = empty($title) ? get_string('helpmenutitle', 'theme_ucsf') : $title;
+
+        $menu['items'] = array();
+        $number_of_links = (int)$this->get_theme_setting('catnumberoflinks' . $category);
+        for ($i = 1; $i <= $number_of_links; $i++) {
+            $url = $this->get_theme_setting('cathelpfeedback' . $i . 'link' . $category);
+            $title = $this->get_theme_setting('cathelpfeedback' . $i . 'linklabel' . $category);
+            $target = $this->get_theme_setting('cathelpfeedback' . $i . 'linktarget' . $category);
+
+            if (!empty($url)) {
+                $menu['items'][] = array(
+                    'url' => $url,
+                    'title' => empty($title) ? '' : $title,
+                    'options' => array(
+                        'target' => empty($target) ? '_self' : '_blank'
+                    ),
+                );
+            }
+        }
+
+        if (empty($menu['items'])) {
+            return false;
+        }
+
+        return $menu;
+    }
+
+    /**
      * @inheritdoc
      */
-    protected function get_home_ref($returnlink = true) {
-        global $CFG, $SITE, $PAGE;
+    protected function get_home_ref($returnlink = true)
+    {
 
-        $theme_settings = $PAGE->theme->settings;
+        $theme_settings = $this->page->theme->settings;
 
         // @todo integrate category overrides [ST 2017/04/18]
 
@@ -87,5 +188,55 @@ class theme_ucsf_core_renderer extends theme_clean_core_renderer {
         }
 
         return html_writer::tag('span', $sitename, array('class' => 'brand'));
+    }
+
+    /**
+     * Retrieves a given theme setting and formats it according to the given formatting option.
+     *
+     * @param string $setting
+     * @param bool|string $format
+     * @return bool|string
+     *
+     * @see get_theme_setting
+     */
+    protected function get_theme_setting($setting, $format = false)
+    {
+        return theme_ucsf_get_setting($setting, $format);
+    }
+
+    /**
+     * @return int
+     *
+     * @see theme_ucsf_get_current_course_category()
+     */
+    protected function get_current_course_category()
+    {
+        global $COURSE;
+        return theme_ucsf_get_current_course_category($this->page, $COURSE);
+    }
+
+
+    /**
+     * @param int $id
+     * @return array
+     *
+     * @see theme_ucsf_get_category_roots()
+     */
+    protected function get_category_roots($id)
+    {
+        return theme_ucsf_get_category_roots($id);
+    }
+
+    /**
+     * @param array $category_hierarchy
+     * @param string $config_key_prefix
+     * @return int
+     *
+     * @see theme_ucsf_find_first_configured_category()
+     */
+    protected function find_first_configured_category(array $category_hierarchy, $config_key_prefix)
+    {
+        $theme_settings = $this->page->theme->settings;
+        return theme_ucsf_find_first_configured_category($theme_settings, $category_hierarchy, $config_key_prefix);
     }
 }
