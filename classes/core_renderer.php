878c5461bcb95a2fe6c1682bd7d401aabe07a557
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-04-25 16:13:35 -0700

    rm unnessary override.

--- classes/core_renderer.php
+++ classes/core_renderer.php
@@ -110,23 +110,4 @@ class theme_ucsf_core_renderer extends theme_clean_core_renderer
     {
         return html_writer::span($title, 'brand');
     }
-
-    /**
-     * @inheritdoc
-     */
-    protected function get_home_ref($returnlink = true)
-    {
-
-        $theme_settings = $this->page->theme->settings;
-
-        // @todo integrate category overrides [ST 2017/04/18]
-
-        $sitename = format_string($theme_settings->headerlabel, true, array('context' => context_course::instance(SITEID)));
-
-        if ($returnlink) {
-            return html_writer::link(new moodle_url('/'), $sitename, array('class' => 'brand', 'title' => get_string('home')));
-        }
-
-        return html_writer::tag('span', $sitename, array('class' => 'brand'));
-    }
 }
