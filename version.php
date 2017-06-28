c5d4879117993b1d6f825bdf4f0752dc7211ac77
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-06-27 17:13:34 -0700

    version and release bump.

--- version.php
+++ version.php
@@ -8,8 +8,9 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2017062700;
 $plugin->requires  = 2016112900;
-$plugin->release = 'v2.0.0-rc4';
+$plugin->release = 'v2.0.0';
 $plugin->component = 'theme_ucsf';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2016112900,
     'theme_clean'  => 2016112900,
