b11726e41c16126e67a5313ea7301c29070c2891
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-12-04 15:12:47 -0800

    version bump.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018112001;
+$plugin->version   = 2018120400;
 $plugin->requires  = 2018050800;
 $plugin->component = 'theme_ucsf';
 
