a8c793dde0d51b85b4a667e20e197c3fc11a54ea
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-12-10 15:55:06 -0800

    tweaking the no-progress icon display in courses list on dashboard.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121001;
+$plugin->version   = 2018121002;
 $plugin->requires  = 2018050800;
 $plugin->component = 'theme_ucsf';
 
