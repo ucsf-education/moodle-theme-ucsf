64b70d9ebadc43d6eb91f247060f5e20a6b18174
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-12-11 15:51:53 -0800

    fixed fat-fingered regression in style sheet.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121104;
+$plugin->version   = 2018121105;
 $plugin->requires  = 2018050800;
 $plugin->component = 'theme_ucsf';
 
