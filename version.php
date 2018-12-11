d6aebc8728a3f4ec33afc4bc0932d006d0d65918
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-12-11 15:48:33 -0800

    squished (almost) all style linter warnings.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121103;
+$plugin->version   = 2018121104;
 $plugin->requires  = 2018050800;
 $plugin->component = 'theme_ucsf';
 
