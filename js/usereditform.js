c6e71d84714b555ba5e38ef37a4b7ebfbf5eab96
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-05 14:41:39 -0800

    target both the admin as well as user profile form.

--- js/usereditform.js
+++ js/usereditform.js
@@ -1,6 +1,9 @@
 // User edit form customizations
 require(['jquery'], function($) {
-    var $elem = $('#page-admin-user-editadvanced #fitem_id_firstname');
+    var $elem = $('#page-user-editadvanced #fitem_id_firstname'); // admin user profile form
+    if (! $elem.length) {
+        $elem = $('#page-user-edit #fitem_id_firstname'); // user profile form
+    }
     if ($elem.length) {
         $elem.prepend('<div class="col-md-3"></div><div class="col-md-9"><a href="https://it.ucsf.edu/how-to/how-update-your-name" target="_blank">How to update name information.</a></div>');
     }
