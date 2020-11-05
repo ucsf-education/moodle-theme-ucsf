fbc0644c7286b8621516c882266bdea9ad9b78ad
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-11-05 14:20:04 -0800

    replaces placeholder text with actual info.

--- js/usereditform.js
+++ js/usereditform.js
@@ -2,6 +2,6 @@
 require(['jquery'], function($) {
     var $elem = $('#page-admin-user-editadvanced #fitem_id_firstname');
     if ($elem.length) {
-        $elem.prepend('<div class="col-md-3"></div><div class="col-md-9">Lorem ipsum dolor sit amet... <a href="#">Link to somewhere</a>.</div>');
+        $elem.prepend('<div class="col-md-3"></div><div class="col-md-9"><a href="https://it.ucsf.edu/how-to/how-update-your-name" target="_blank">How to update name information.</a></div>');
     }
 });
