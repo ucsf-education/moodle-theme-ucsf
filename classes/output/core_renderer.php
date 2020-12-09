605000385781f0272e0d3adcc6c468184a0d4566
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-09 13:26:11 -0800

    removes obsolete full_header renderer function override.

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -179,37 +179,6 @@ class core_renderer extends \theme_boost\output\core_renderer
     }
 
     /**
-     * @inheritdoc
-     * @throws \coding_exception
-     * @throws \moodle_exception
-     */
-    public function full_header() {
-        global $CFG, $COURSE, $USER, $PAGE;
-
-        if ($this->page->include_region_main_settings_in_header_actions() &&
-            !$this->page->blocks->is_block_present('settings')) {
-            // Only include the region main settings if the page has requested it and it doesn't already have
-            // the settings block on it. The region main settings are included in the settings block and
-            // duplicating the content causes behat failures.
-            $this->page->add_header_action(html_writer::div(
-                $this->region_main_settings_menu(),
-                'd-print-none',
-                ['id' => 'region-main-settings-menu']
-            ));
-        }
-
-        $header = new stdClass();
-        $header->settingsmenu = $this->context_header_settings_menu();
-        $header->contextheader = $this->context_header();
-        $header->hasnavbar = empty($this->page->layout_options['nonavbar']);
-        $header->navbar = $this->navbar();
-        $header->pageheadingbutton = $this->page_heading_button();
-        $header->courseheader = $this->course_header();
-        $header->headeractions = $this->page->get_header_actions();
-        return $this->render_from_template('core/full_header', $header);
-    }
-
-    /**
      * Build the guest access hint HTML code.
      *
      * @param int $courseid The course ID.
