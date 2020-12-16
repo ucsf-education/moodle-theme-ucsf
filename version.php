53e8ec2bdf6b387f8b2dfa1e0e10679bc9f26064
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-16 14:55:03 -0800

    color correction for focused/hovered custom menu items.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020121000;
+$plugin->version   = 2020121600;
 $plugin->requires  = 2020060900;
 $plugin->component = 'theme_ucsf';
 
