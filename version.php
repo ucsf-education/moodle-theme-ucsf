dd0c49b00dc4564050283901d192dc60c8859638
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-21 11:16:33 -0800

    applies custom styles to icons in dropdown menu.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020121600;
+$plugin->version   = 2020122100;
 $plugin->requires  = 2020060900;
 $plugin->component = 'theme_ucsf';
 
