9568eeb9361f5dea0f82d04c42d46106e3c2aed7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-03-08 19:31:13 -0800

    corrects top padding of custom alerts container.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021030100;
+$plugin->version   = 2021030800;
 $plugin->requires  = 2020110300;
 $plugin->component = 'theme_ucsf';
 
