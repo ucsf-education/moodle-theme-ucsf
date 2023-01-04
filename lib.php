fa73d1bdf355278f5e1f92337597581b3396f582
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-01-04 10:55:30 -0800

    adds utility methods for dealing with theme settings and category customizations back in.

--- lib.php
+++ lib.php
@@ -79,3 +79,160 @@ function theme_ucsf_get_main_scss_content($theme) {
     // Combine them together.
     return $pre . "\n" . $scss . "\n" . $post;
 }
+
+/**
+ * Retrieves a theme setting.
+ *
+ * @param stdClass $theme_settings The theme settings object
+ * @param string   $setting        The name of the setting.
+ * @param mixed    $default        A default value, to be used as fallback if the setting is not defined.
+ *
+ * @return mixed The setting's value, or the given default if the setting has not been defined.
+ */
+function _theme_ucsf_get_setting($theme_settings, $setting, $default = false)
+{
+    if (! isset($theme_settings->$setting)) {
+        return $default;
+    }
+
+    return $theme_settings->$setting;
+}
+
+/**
+ * Recursively retrieve all ancestral categories for a given category, including the category itself.
+ *
+ * @param int $id The category id.
+ * @param array $categories A partial list of ancestral category ids.
+ *
+ * @return array A list full list of ancestral category ids, including the given id itself.
+ * @throws dml_exception
+ */
+function _theme_ucsf_recursively_get_category_roots($id, $categories = array())
+{
+    global $DB;
+
+    $sql = "SELECT cc.parent, cc.name FROM {course_categories} cc WHERE cc.id = ?";
+    $cats = $DB->get_records_sql($sql, array($id));
+
+    if (empty($cats)) {
+        return $categories;
+    }
+
+    $categories[] = $id;
+    $cat = array_shift($cats);
+
+    return _theme_ucsf_recursively_get_category_roots($cat->parent, $categories);
+}
+
+/**
+ * Returns a list of all ancestral categories of a given category.
+ * The first element in that list is the given category itself, followed by its parent, the parent's parent and so on.
+ *
+ * @param int $id The category id.
+ *
+ * @return array A list of category ids, will be empty if the given category is bogus.
+ * @throws dml_exception
+ */
+function _theme_ucsf_get_category_roots($id)
+{
+    static $cache = null;
+
+    if (! isset($cache)) {
+        $cache = array();
+    }
+
+    if (! array_key_exists($id, $cache)) {
+        $ids = _theme_ucsf_recursively_get_category_roots($id);
+        $cache[$id] = _theme_ucsf_recursively_get_category_roots($id);
+        array_shift($ids);
+        // cache category roots of all ancestors in that category hierarchy while at it.
+        for ($i = 0, $n = count($ids); $i < $n; $i++) {
+            $parent_id = $ids[$i];
+            if (array_key_exists($parent_id, $cache)) {
+                break;
+            }
+            $cache[$parent_id] = array_slice($ids, $i);
+        }
+    }
+
+    return $cache[$id];
+}
+
+
+/**
+ * Retrieves the current course category id.
+ *
+ * @param moodle_page $page   The current page object.
+ * @param stdClass    $course The current course object.
+ *
+ * @return string The course category id.
+ */
+function _theme_ucsf_get_current_course_category(moodle_page $page, $course)
+{
+    // ACHTUNG!
+    // Unbelievably crappy code to follow.
+    // For course category pages, peel the category out of the URL request parameter.
+    // In all other cases, take it from the current course.
+    // @todo Clean this horrid mess up [ST 2016/03/24]
+    if ($page->pagelayout == "coursecategory" && isset($_REQUEST["categoryid"])) {
+        return $_REQUEST["categoryid"];
+    }
+
+    return $course->category;
+}
+
+/**
+ * Retrieve a list of all course category ids,
+ * since Moodle's course API does not appear to provide such a method.
+ *
+ * @return array A list course ids, sorted by ID in descending order (newest first).
+ *
+ * @throws dml_exception
+ */
+function _theme_ucsf_get_all_category_ids()
+{
+    global $DB;
+
+    $sql = "SELECT cc.id FROM {course_categories} cc ORDER BY cc.id DESC";
+    $categories = array_keys($DB->get_records_sql($sql));
+
+    return $categories;
+}
+
+/**
+ * Find and returns the first category (from the bottom) in a given category hierarchy
+ * that has a customized setting in a given theme.
+ *
+ * Example:
+ *  1. The category hierarchy is (top) id = 1 >> id = 2 >> id = 5 >> id = 7 (bottom).
+ *  2. We're searching the theme settings for all entries pertaining to custom labels (all config keys starting with "customlabel").
+ *  3. The theme settings contains entries keyed of by 'customlabel1' an 'customlabel5'.
+ *  4. This method will return 5, since 'customlabel5' matches the lowest category id = 5 in the hierarchy.
+ *
+ * @param object $theme_settings     The theme settings.
+ * @param array  $category_hierarchy A hierarchy of category ids, sorted bottom to top.
+ * @param string $config_key_prefix  Configuration settings key prefix.
+ *
+ * @return int The first matching category id. 0 if no matching category can be found.
+ * @see _theme_ucsf_get_category_roots()
+ */
+function _theme_ucsf_find_first_configured_category($theme_settings, array $category_hierarchy, $config_key_prefix)
+{
+    // get a list of all categories that have customizations enabled.
+    $enabled_categories = array();
+    if (! empty($theme_settings->all_categories)) {
+        $enabled_categories = explode(",", $theme_settings->all_categories);
+    }
+
+    // find first matching
+    foreach ($category_hierarchy as $category_id) {
+        if (in_array($category_id, $enabled_categories)) {
+            $config_key = $config_key_prefix . $category_id;
+            if (! empty($theme_settings->$config_key)) {
+                return $category_id;
+            }
+        }
+    }
+
+    return 0;
+}
