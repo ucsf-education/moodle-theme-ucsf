a2410b4b740de965fd1eda3d1ee084c8984b1538
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-01-13 15:56:50 -0800

    appease the js linter.

--- js/usereditform.js
+++ js/usereditform.js
@@ -1,10 +1,12 @@
 // User edit form customizations
 require(['jquery'], function($) {
-    var $elem = $('#page-user-editadvanced #fitem_id_firstname'); // admin user profile form
-    if (! $elem.length) {
-        $elem = $('#page-user-edit #fitem_id_firstname'); // user profile form
+    var $elem = $('#page-user-editadvanced #fitem_id_firstname'); // Admin user profile form
+    if (!$elem.length) {
+        $elem = $('#page-user-edit #fitem_id_firstname'); // User profile form
     }
     if ($elem.length) {
-        $elem.prepend('<div class="col-md-3"></div><div class="col-md-9"><a href="https://it.ucsf.edu/how-to/how-update-your-name" target="_blank">How to update name information.</a></div>');
+        $elem.prepend('<div class="col-md-3"></div><div class="col-md-9">'
+            + '<a href="https://it.ucsf.edu/how-to/how-update-your-name" target="_blank">How to update name information.</a>'
+            + '</div>');
     }
 });
