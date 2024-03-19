4ff51ffa13c855c6563d6042c68908440a2de2a5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-03-18 18:17:39 -0700

    sub-class Boost's step definitions for navigation so we can reuse them.

---
+++ tests/behat/behat_theme_ucsf_behat_navigation.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Behat navigation-related step definition overrides for the UCSF theme.
+ *
+ * @package theme_ucsf
+ * @copyright The Regents of the University of California
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../boost/tests/behat/behat_theme_boost_behat_navigation.php');
+
+/**
+ * Navigation-related step definition overrides for the UCSF theme.
+ *
+ * @package theme_ucsf
+ * @copyright The Regents of the University of California
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_theme_ucsf_behat_navigation extends behat_theme_boost_behat_navigation {
+    // We need to define our own subclass here in order to use Boost's step definitions.
+}
