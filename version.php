5c5b67764d6c8535c55ce142a9f56c3c3bb0e459
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-05-06 12:15:13 -0700

    rm obsolete custom icon.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_ucsf';
-$plugin->version = 2025021403;
-$plugin->release = 'v4.5.2';
+$plugin->version = 2025021404;
+$plugin->release = 'v4.5.3';
 $plugin->requires = 2024100100;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
